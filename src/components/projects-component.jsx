4ca8748111729fe46e03f6f0e9f30b64021e34f0
KhanStan99 <soubankhan3@gmail.com>
2020-06-19 19:54:46 +0530

    Added Projects & Chips

--- src/components/projects-component.jsx
+++ src/components/projects-component.jsx
@@ -7,45 +7,95 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Chip from "@material-ui/core/Chip";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   media: {
-    height: 140,
+    height: 240,
   },
-});
+  chip: {
+    display: "flex",
+    color: "#FFF",
+    justifyContent: "start",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
+  },
+  cards: {
+    display: "flex",
+    justifyContent: "start",
+    flexWrap: "wrap",
+    marginBottom: theme.spacing(1.5),
+    textAlign: "start",
+  },
+}));
 
 export default function ProjectsComponent() {
   const classes = useStyles();
 
   const projects = [
     {
-      title: "Android Project",
+      title: "Covid 19 Tracker",
       image:
-        "https://www.edureka.co/blog/wp-content/uploads/2019/10/Android-Projects.png",
+        "https://static.wixstatic.com/media/8443ed_907b0743b641488abe35a9947646ee03~mv2.jpg/v1/fill/w_1000,h_563,al_c,q_90,usm_0.66_1.00_0.01/8443ed_907b0743b641488abe35a9947646ee03~mv2.jpg",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porttitor sapien at tortor fermentum congue. Integer dui est, eleifend at sagittis vel",
-      target: "http://www.google.com",
+        "In December 2019, a novel coronavirus strain (SARS-CoV-2) emerged in the city of Wuhan, China. I created a tracking application specifically for Indian regions which shows detailed list of cases from all over India.",
+      target: [
+        {
+          label: "View on Github",
+          target: "https://github.com/KhanStan99/Covid-19Tracker-ReactJS",
+        },
+        {
+          label: "View on Web",
+          target: "https://cv19tracker.firebaseapp.com/",
+        },
+      ],
+      tags: ["ReactJS", "Material UI", "Java Script", "HTMl/CSS"],
     },
     {
-      title: "ReactJS Project",
-      image: "https://miro.medium.com/max/2674/1*o5JioPIf2Y7fwXWvhLf_CA.png",
+      title: "Angular Login Structure",
+      image: "https://miro.medium.com/max/3492/1*P7x-_0XfQz6CVmMY_QAv0w.png",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porttitor sapien at tortor fermentum congue.",
-      target: "http://www.google.com",
+        "Its a basic Angular application which show how to maintain user session and route from login to home screen by notifying other components",
+      target: [
+        {
+          label: "View on Github",
+          target: "https://github.com/KhanStan99/Angular-Basic-Login-Structure",
+        },
+      ],
+      tags: ["Angular", "Type Script", "HTMl/CSS"],
     },
     {
-      title: "My Project Three",
-      image:
-        "https://us.123rf.com/450wm/sunil4rnz4ever/sunil4rnz4ever1803/sunil4rnz4ever180301261/100284768-beautiful-scenari-in-norway.jpg?ver=6",
-      description: "blah blah",
-      target: "http://www.google.com",
+      title: "Android MVVM Structure",
+      image: "https://miro.medium.com/max/960/1*kWwjlkOEyTV6M7W7tZrs1w.png",
+      description:
+        "Android project MVVM structure. You can use this to avoid writing boiler plate code for your new projects.",
+      target: [
+        {
+          label: "View on Github",
+          target: "https://github.com/KhanStan99/AndroidMVVMBaseStructure",
+        },
+      ],
+      tags: ["Android", "MVVM", "Kotlin", "XML"],
     },
     {
-      title: "My Project Four",
-      image:
-        "https://us.123rf.com/450wm/sunil4rnz4ever/sunil4rnz4ever1803/sunil4rnz4ever180301261/100284768-beautiful-scenari-in-norway.jpg?ver=6",
-      description: "blah blah",
-      target: "http://www.google.com",
+      title: "Android MVVM Structure",
+      image: "https://miro.medium.com/max/960/1*kWwjlkOEyTV6M7W7tZrs1w.png",
+      description:
+        "Android project MVVM structure. You can use this to avoid writing boiler plate code for your new projects.",
+      target: [
+        {
+          label: "View on Github",
+          target: "https://github.com/KhanStan99/AndroidImageCompressor",
+        },
+        {
+          label: "View on Play Store",
+          target:
+            "https://play.google.com/store/apps/details?id=in.trentweet.imagecompressor",
+        },
+      ],
+      tags: ["Android", "MVVM", "Kotlin", "XML"],
     },
   ];
 
@@ -55,7 +105,7 @@ export default function ProjectsComponent() {
 
   return projects.map((item) => {
     return (
-      <Card key={item.title} style={{ textAlign: "start" }}>
+      <Card key={item.title} className={classes.cards}>
         <CardActionArea>
           <CardMedia
             className={classes.media}
@@ -70,17 +120,26 @@ export default function ProjectsComponent() {
               {item.description}
             </Typography>
           </CardContent>
+          <div className={classes.chip}>
+            {item.tags.map((tag) => {
+              return <Chip label={tag} key={tag} />;
+            })}
+          </div>
         </CardActionArea>
         <CardActions>
-          <Button
-            size="small"
-            color="primary"
-            onClick={() => {
-              openProjects(item.target);
-            }}
-          >
-            View on Github
-          </Button>
+          {item.target.map((target) => {
+            return (
+              <Button
+                size="small"
+                color="primary"
+                onClick={() => {
+                  openProjects(target.target);
+                }}
+              >
+                {target.label}
+              </Button>
+            );
+          })}
         </CardActions>
       </Card>
     );
